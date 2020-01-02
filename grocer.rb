3a32296347ae335136a6b04e89e6c19ec7a66c7e
Natalia  Wit <natwit95@gmail.com>
2020-01-02 23:58:01 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -66,18 +66,17 @@ def apply_clearance(cart)
   # REMEMBER: This method **should** update cart
 
   i = 0
-  while i < cart.length do
-    item = cart[i]
-    if item[:clearance]
-      discounted_price = ((1 - CLEARANCE_ITEM_DISCOUNT_RATE) * item[:price]).round(2)
-        item[:price] = discounted_price
+    while i < cart.length do
+      item = cart[i]
+      if item[:clearance]
+        discounted_price = ((1 - CLEARANCE_ITEM_DISCOUNT_RATE) * item[:price]).round(2)
+          item[:price] = discounted_price
+      end
+      i += 1
     end
-    i += 1
-  end
 
-  cart
+    cart
 end
-
 
 def checkout(cart, coupons)
   # Consult README for inputs and outputs
