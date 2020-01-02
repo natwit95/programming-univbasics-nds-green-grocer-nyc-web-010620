bb1bb4dbe5422a61e01dd89741b8d97c7289ae83
Natalia  Wit <natwit95@gmail.com>
2020-01-02 23:57:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -44,12 +44,20 @@ def apply_coupons(cart, coupons)
   #
   # REMEMBER: This method **should** update cart
 
-  rounded_unit_price = (c[:cost].to_f * 1.0 / c[:num]).round(2)
- {
-   :item => "#{c[:item]} W/COUPON",
-   :price => rounded_unit_price,
-   :count => c[:num]
- }
+  i = 0
+   while i < coupons.count do
+     coupon = coupons[i]
+     item_with_coupon = find_item_by_name_in_collection(coupon[:item], cart)
+     item_is_in_basket = !!item_with_coupon
+     count_is_big_enough_to_apply = item_is_in_basket && item_with_coupon[:count] >= coupon[:num]
+
+     if item_is_in_basket and count_is_big_enough_to_apply
+       apply_coupon_to_cart(item_with_coupon, coupon, cart)
+     end
+     i += 1
+   end
+
+   cart
 end
 
 def apply_clearance(cart)
