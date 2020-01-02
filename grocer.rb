675444da637b0eb023896550d79ad6dfaf214e34
Natalia  Wit <natwit95@gmail.com>
2020-01-02 23:51:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -18,6 +18,25 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+
+  def consolidate_cart(cart)
+  i = 0
+  result = []
+
+  while i < cart.count do
+    item_name = cart[i][:item]
+    sought_item = find_item_by_name_in_collection(item_name, result)
+    if sought_item
+      sought_item[:count] += 1
+    else
+      cart[i][:count] = 1
+      result << cart[i]
+    end
+    i += 1
+  end
+
+  result
+end
 end
 
 def apply_coupons(cart, coupons)
