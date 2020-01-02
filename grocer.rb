c789327557aaa99ef2836a645ebba51c8fe09960
Natalia  Wit <natwit95@gmail.com>
2020-01-02 23:53:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -19,7 +19,7 @@ def consolidate_cart(cart)
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 
-  
+
   i = 0
   result = []
 
